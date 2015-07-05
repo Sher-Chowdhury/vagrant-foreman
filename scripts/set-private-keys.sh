8bc434fd4827f21e565e6f65db58defe39a583a9
Sher <sher.chowdhury@gmail.com>
2015-07-05 15:50:20 +0100

    switched file format to unix

---
+++ scripts/set-private-keys.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if ![ -d /home/vagrant/.ssh ]; then
+  mkdir -p /home/vagrant/.ssh
+  chown vagrant:vagrant /home/vagrant/.ssh
+fi
+if ![ -f /home/vagrant/.ssh/authorized_keys ]; then
+  wget --no-check-certificate https://raw.github.com/mitchellh/
+  vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
+  chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+fi
+echo "vagrant ALL=(ALL) NOPASSWD: ALL\n" >> /etc/sudoers
