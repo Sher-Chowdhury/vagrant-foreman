61489bcc2759c601a14f2d530cf8da2993c99a11
Sher <sher.chowdhury@gmail.com>
2015-07-11 17:46:47 +0100

    foreman is installing, but can't login successfully yet.

--- Vagrantfile
+++ Vagrantfile
@@ -3,20 +3,35 @@
 
 
 Vagrant.configure(2) do |config|
-
-  config.vm.define "puppetmaster" do |puppetmaster|
-    puppetmaster.vm.box = "centos7.box"
+  # The "puppetmaster" string is the name of the box. hence you can do "vagrant up puppetmaster"
+  config.vm.define "puppetmaster" do |puppetmaster_config|
+    puppetmaster_config.vm.box = "centos7.box"
     
-    puppetmaster.vm.provider "virtualbox" do |vb|
+	# this set's the machine's hostname. 
+	puppetmaster_config.vm.hostname = "puppet.master"  
+
+	# this let's you access the machine httpd service from the host machine.
+	puppetmaster_config.vm.network "forwarded_port", guest:80, host:8888
+	
+	puppetmaster_config.vm.network "private_network", ip: "192.168.50.4"
+
+    puppetmaster_config.vm.provider "virtualbox" do |vb|
       # Display the VirtualBox GUI when booting the machine
       vb.gui = true
       
       # Customize the amount of memory on the VM:
       vb.memory = "1024"
-    
-      vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+ 
+      # name of machine that appears on the vb console and vb consoles title. 	  
+	  vb.name = "centos7-foreman"   
+	
+      vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]  
     end
     
+	
+	puppetmaster_config.vm.provision "shell", path: "scripts/foreman-install.sh"
+	
+	
   end
   
 end
--- scripts/foreman-install.sh
+++ scripts/foreman-install.sh
@@ -1,67 +1,10 @@
 #!/bin/sh
 
-if ps -ef | grep "/usr/share/foreman" | grep -v grep 2> /dev/null
-then
-    echo "Foreman appears to all already be installed. Exiting..."
-	exit 0
-fi
 
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum -y install epel-release http://yum.theforeman.org/releases/1.8/el7/x86_64/foreman-release.rpm
 yum -y install foreman-installer
 
+yum -y update
 
-echo "  "
-echo "  "
-echo "  "
-echo "  "
-
-echo "FACTER FQDN VALUE IS EQUAL TO:"
-facter fqdn
-
-echo "HOSTNAME FQDN VALUE IS EQUAL TO:"
-hostname -f
-
-
-if [ -z `facter domain` ]; then
-  hostname "localhost.test"
-  echo "localhost.test" > /etc/hostname
-else
-  hostname "localhost.`facter domain`"
-  echo "localhost.`facter domain`" > /etc/hostname
-fi 
-
-
-echo "HOSTNAME FQDN VALUE IS NOW EQUAL TO:"
-hostname -f
-
-
-echo "CONTENT OF /ETC/HOSTS:"
-cat /etc/hosts
-
-
-echo "CONTENT OF /etc/sysconfig/network:"
-cat /etc/sysconfig/network
-
-echo "  "
-echo "  "
-echo "  "
-echo "  "
-echo "  "
-
-
-# might need to permenantly change the hostname value using the "hostname" command. 
-# Make sure above command gives the same output. If needed, change the hostname permanently via 'hostname' command 
-# and editing appropriate configuration file(e.g. on Red Hat systems /etc/sysconfig/network). 
-# If 'hostname -f' still returns unexpected result, check /etc/hosts and put hostname entry in the 
-# correct order, for example
-# 1.2.3.4 full.hostname.com full
-
-# Fully qualified hostname must be the first entry on the line
-#https://www.google.co.uk/search?q=Make+sure+above+command+gives+the+same+output.+If+needed%2C+change+the+hostname+permanently+via+%27hostname%27+command&oq=Make+sure+above+command+gives+the+same+output.+If+needed%2C+change+the+hostname+permanently+via+%27hostname%27+command&aqs=chrome..69i57.246j0j7&sourceid=chrome&es_sm=122&ie=UTF-8
-
-
-foreman-installer \
---foreman-admin-username=vagrant \
---foreman-admin-password=vagrant
-
+foreman-installer
