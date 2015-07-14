e53c23ad3f4394c51161fafb596a663749964637
Sher <sher.chowdhury@gmail.com>
2015-07-14 22:28:06 +0100

    created r10k.yaml config file

--- scripts/install-gems.sh
+++ scripts/install-gems.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 gem install r10k --no-ri --no-rdoc
-mkdir /etc/puppetlabs/
-cp /vagrant/files/r10k.yaml /etc/puppet/
+mkdir -p /etc/puppetlabs/r10k
+cp /vagrant/files/r10k.yaml /etc/puppetlabs/r10k/
+
 
 gem install bundler --no-ri --no-rdoc
 gem install rake --no-ri --no-rdoc
