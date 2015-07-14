415493a45f67d9d14ec342df90c43fe4a06ca27e
Sher <sher.chowdhury@gmail.com>
2015-07-14 19:37:29 +0100

    getting r10k working

--- scripts/install-gems.sh
+++ scripts/install-gems.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 gem install r10k --no-ri --no-rdoc
+mkdir /etc/puppetlabs/
+cp /vagrant/files/r10k.yaml /etc/puppet/
+
 gem install bundler --no-ri --no-rdoc
 gem install rake --no-ri --no-rdoc
+
+
