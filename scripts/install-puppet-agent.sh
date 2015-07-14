fb58f6f83c2753d25a42203b83caab6c5425de70
Sher <sher.chowdhury@gmail.com>
2015-07-14 17:33:15 +0100

    host appears on foreman's guid when doing a puppet run on the agent

--- scripts/install-puppet-agent.sh
+++ scripts/install-puppet-agent.sh
@@ -4,4 +4,5 @@ rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 
 yum -y install puppet facter
 
-
+echo "    certname          = `hostname --fqdn`" >> /etc/puppet/puppet.conf
+echo "    server            = puppetmaster.local" >> /etc/puppet/puppet.conf
