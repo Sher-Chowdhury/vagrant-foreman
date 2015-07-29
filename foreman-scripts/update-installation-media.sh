0a2109b308b15f12448afa06b2a4fc8707dc3e69
Sher <sher.chowdhury@gmail.com>
2015-07-29 14:06:10 +0100

    updated foreman's install media script

--- foreman-scripts/update-installation-media.sh
+++ foreman-scripts/update-installation-media.sh
@@ -9,4 +9,10 @@ hammer medium delete --name 'FreeBSD mirror'
 hammer medium delete --name 'OpenSUSE mirror'
 hammer medium delete --name 'Ubuntu mirror'
 
+
+hammer medium create --name "OracleLinux Mirror" --path "http://`hostname -f`/mirror/OracleLinux/OL\$major/\$minor/base/\$arch"
+
+# This gets resolved to numerous paths, here's an example to one of them:
+# http://fqdn/mirror/OracleLinux/OL6/6/base/x86_64/
+
 exit 0
