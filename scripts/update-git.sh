8e2bf3528bfe685ac474a0fab612b8d87c16e89e
Khris Leong <khris.leong@os.uk>
2015-07-30 12:22:04 +0100

    Added git update script to update git 1.9.4

--- scripts/update-git.sh
+++ scripts/update-git.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 
+yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker
 
-#git config --global push.default simple
+cd /usr/src
+wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz
+tar xzf git-1.9.4.tar.gz
+
+cd git-1.9.4
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+echo "export PATH=/usr/local/git/bin:$PATH" >> /etc/bashrc
+source /etc/bashrc
