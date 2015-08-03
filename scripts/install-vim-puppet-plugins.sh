cc13cda023d13589ff943bbc7b9ea3a9f8df9389
Sher <sher.chowdhury@gmail.com>
2015-08-04 00:15:27 +0100

    fixed bug and added snipmate and vim snippet plugins

--- scripts/install-vim-puppet-plugins.sh
+++ scripts/install-vim-puppet-plugins.sh
@@ -15,17 +15,26 @@ git clone https://github.com/scrooloose/syntastic.git
 git clone git://github.com/rodjek/vim-puppet.git
 git clone git://github.com/godlygeek/tabular.git
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/honza/vim-snippets.git
 
-echo "execute pathogen#infect()"
-echo "syntax on"
-echo "filetype plugin indent on"
-echo "filetype on"
-echo "set statusline+=%#warningmsg#"
-echo "set statusline+=%{SyntasticStatuslineFlag()}"
-echo "set statusline+=%*"
-echo "let g:syntastic_always_populate_loc_list = 1"
-echo "let g:syntastic_auto_loc_list = 1"
-echo "let g:syntastic_check_on_open = 1"
-echo "let g:syntastic_check_on_wq = 1"
+
+# vim snippets and shipmate:
+
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+git clone https://github.com/honza/vim-snippets.git
+
+echo "execute pathogen#infect()" > ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+echo "filetype plugin indent on" >> ~/.vimrc
+echo "filetype on" >> ~/.vimrc
+echo "set statusline+=%#warningmsg#" >> ~/.vimrc
+echo "set statusline+=%{SyntasticStatuslineFlag()}" >> ~/.vimrc
+echo "set statusline+=%*" >> ~/.vimrc
+echo "let g:syntastic_always_populate_loc_list = 1" >> ~/.vimrc
+echo "let g:syntastic_auto_loc_list = 1" >> ~/.vimrc
+echo "let g:syntastic_check_on_open = 1" >> ~/.vimrc
+echo "let g:syntastic_check_on_wq = 1" >> ~/.vimrc
 
 
