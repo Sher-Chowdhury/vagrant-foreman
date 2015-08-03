61ee828bf1ce6311dcdd6ccf88f6fba672e55050
Sher <sher.chowdhury@gmail.com>
2015-08-03 13:10:06 +0100

    docker script changes

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+# https://docs.docker.com/installation/centos/#manual-installation-of-latest-docker-release
 
+# install docker 
+# the docker rpm should be available in the core OS repos. 
 yum -y install docker
 
 systemctl start docker
 
 systemctl enable docker
+
+# give vagrant user full priveleges to use docker
+
+
+usermod -aG docker vagrant
