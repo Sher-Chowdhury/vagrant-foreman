9bb68a12cf511308879e6892236a753c154dfd13
Sher <sher.chowdhury@gmail.com>
2015-08-05 14:54:55 +0100

    yet more docker stuff

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -28,26 +28,34 @@ cat /etc/group | grep docker   # This is to confirm that the docker group exists
 groupadd docker                
 gpasswd -a vagrant docker      # If you are running this manually then you need to restart the terminal session to clear the old cache. 
 
-docker run -it centos ls       # this will take awhile, about 5mins. Because it is downloading the image from docker.io website for the first time. 
-docker run -it centos ls       # This should 
-
+docker run centos ls       # this will take awhile, about 5mins. Because it is downloading the image from docker.io website for the first time. 
+docker run centos ls       # This should 
+# If you want to run the above command manually then also specify the "-it" options. for (i)nteractive and (t)erminal, e.g. :   docker -it run centos ls
 
 netstat -tlp | grep docker   # By default, the docker service listens to a socket port. 
                              # So nothing will get outputted here. 
 							 # But we want it to also listen on a network port too. 
 
+# Needed to do a network reset here. Not sure whey though. 
+systemctl restart NetworkManager
+systemctl restart network							 
+							 
+							 
 # first we need to stop docker: 
 systemctl stop docker       
 
 # Then work out what IP address docker should be listening on.  
 ipaddress=`ip addr show | grep 192 | awk '{print $2}' | cut -d'/' -f1`
 
+
+
 # Now start the docker server. 
 # Port 2375 is the standard port that docker listens on. 
 # We also specify "-d" to indicate that it needs to be started up as a deamon. 
 # We also "$" so that it starts as a background process and we get the shell back and it doesn't hang. 
 # Notice that we specified to "-H" to make our single docker service listen on two sources, the web socket, and the network port. 
-docker -H ${ipaddress}:2375	-H unix:///var/run/docker.sock  -d &		
+docker -H ${ipaddress}:2375 -H unix:///var/run/docker.sock -d &		
+
 
 
 
